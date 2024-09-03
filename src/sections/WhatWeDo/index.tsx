34ef5268451641bbc4dc159b6f25442264f68b18
nadjib-boum <nadjib.dev22@gmail.com>
2024-09-03 09:48:53 +0400

    save

--- src/sections/WhatWeDo/index.tsx
+++ src/sections/WhatWeDo/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import gsap from "gsap";
 import style from "./style.module.css";
 import arrow from "../../assets/arrow-icon.png"
